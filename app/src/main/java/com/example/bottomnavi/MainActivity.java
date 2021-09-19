ace4afac6c21614fdad287cff19ff8ac757f7151
hee jae <hjhjhjhj3770@naver.com>
2021-09-19 20:33:25 +0900

    test1

--- app/src/main/java/com/example/bottomnavi/MainActivity.java
+++ app/src/main/java/com/example/bottomnavi/MainActivity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-//test
+//test1
     private BottomNavigationView bottomNavigationView;
     private FragmentManager fm;
     private FragmentTransaction ft;
